0e1d6e1d312dec79d8bf9540d4225362cbf143bb
sarthakpati <sarthak.pati@hotmail.com>
2021-07-26 10:01:48 -0400

    using validator

--- ScreenshotMaker/screenshot_maker.py
+++ ScreenshotMaker/screenshot_maker.py
@@ -1,13 +1,17 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
+from .utils import perform_sanity_check_on_subject
 
 
 class ScreenShotMaker:
-    def __init__(self, images, masks, slice_numbers, mask_opacity=100):
+    def __init__(self, images, masks=None, slice_numbers=None, mask_opacity=100):
         self.images = images
         self.masks = masks
         self.slice_numbers = slice_numbers
         self.mask_opacity = mask_opacity
+
+    def check_validity(self):
+        return perform_sanity_check_on_subject(self.images, self.masks)
 
     def save_screenshot(self, filename):
         # save the screenshot to a file
