de379ab130f3d2ce64364a7051943451fc103c17
sarthakpati <sarthak.pati@hotmail.com>
2021-07-26 13:41:19 -0400

    simpler, because we don't need the ui stuff

--- ScreenshotMaker/multi_image_display.py
+++ ScreenshotMaker/multi_image_display.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import SimpleITK as sitk
 
 
 class MultiImageDisplay(object):
@@ -35,9 +36,7 @@ class MultiImageDisplay(object):
         horizontal=True,
     ):
 
-        self.npa_list, wl_range, wl_init = self.get_window_level_numpy_array(
-            image_list, window_level_list, intensity_slider_range_percentile
-        )
+        self.npa_list = list(map(sitk.GetArrayFromImage, image_list))
         if title_list:
             if len(image_list) != len(title_list):
                 raise ValueError("Title list and image list lengths do not match")
@@ -48,10 +47,6 @@ class MultiImageDisplay(object):
         # Our dynamic slice, based on the axis the user specifies
         self.slc = [slice(None)] * 3
         self.axis = axis
-
-        # from IPython.display import display
-        # ui = self.create_ui(shared_slider, wl_range, wl_init)
-        # # display(ui)
 
         # Create a figure.
         col_num, row_num = (len(image_list), 1) if horizontal else (1, len(image_list))
@@ -72,135 +67,5 @@ class MultiImageDisplay(object):
                 vmin=wl_slider.value[0],
                 vmax=wl_slider.value[1],
             )
-        self.update_display()
         plt.tight_layout()
         plt.savefig(os.path.join(output_dir, "output.png"))
-
-    def create_ui(self, shared_slider, wl_range, wl_init):
-        # Create the active UI components. Height and width are specified in 'em' units. This is
-        # a html size specification, size relative to current font size.
-
-        if shared_slider:
-            # Validate that all the images have the same size along the axis which we scroll through
-            sz = self.npa_list[0].shape[self.axis]
-            for npa in self.npa_list:
-                if npa.shape[self.axis] != sz:
-                    raise ValueError(
-                        "Not all images have the same size along the specified axis, cannot share slider."
-                    )
-
-            slider = widgets.IntSlider(
-                description="image slice:",
-                min=0,
-                max=sz - 1,
-                step=1,
-                value=int((sz - 1) / 2),
-                width="20em",
-            )
-            slider.observe(self.on_slice_slider_value_change, names="value")
-            self.slider_list = [slider] * len(self.npa_list)
-            slicer_box = widgets.Box(padding=7, children=[slider])
-        else:
-            self.slider_list = []
-            for npa in self.npa_list:
-                slider = widgets.IntSlider(
-                    description="image slice:",
-                    min=0,
-                    max=npa.shape[self.axis] - 1,
-                    step=1,
-                    value=int((npa.shape[self.axis] - 1) / 2),
-                    width="20em",
-                )
-                slider.observe(self.on_slice_slider_value_change, names="value")
-                self.slider_list.append(slider)
-            slicer_box = widgets.Box(padding=7, children=self.slider_list)
-        self.wl_list = []
-        # Each image has a window-level slider, but it is disabled if the image
-        # is a color image len(npa.shape)==4 . This allows us to display both
-        # color and grayscale images in the same UI while retaining a reasonable
-        # layout for the sliders.
-        for r_values, i_values, npa in zip(wl_range, wl_init, self.npa_list):
-            wl_range_slider = widgets.IntRangeSlider(
-                description="intensity:",
-                min=r_values[0],
-                max=r_values[1],
-                step=1,
-                value=[i_values[0], i_values[1]],
-                width="20em",
-                disabled=len(npa.shape) == 4,
-            )
-            wl_range_slider.observe(self.on_wl_slider_value_change, names="value")
-            self.wl_list.append(wl_range_slider)
-        wl_box = widgets.Box(padding=7, children=self.wl_list)
-        return widgets.VBox(children=[slicer_box, wl_box])
-
-    def get_window_level_numpy_array(
-        self, image_list, window_level_list, intensity_slider_range_percentile
-    ):
-        # Using GetArray and not GetArrayView because we don't keep references
-        # to the original images. If they are deleted outside the view would become
-        # invalid, so we use a copy wich guarentees that the gui is consistent.
-        npa_list = list(map(sitk.GetArrayFromImage, image_list))
-
-        wl_range = []
-        wl_init = []
-        # We need to iterate over the images because they can be a mix of
-        # grayscale and color images. If they are color we set the wl_range
-        # to [0,255] and the wl_init is equal, ignoring the window_level_list
-        # entry.
-        for i, npa in enumerate(npa_list):
-            if len(npa.shape) == 4:  # color image
-                wl_range.append((0, 255))
-                wl_init.append((0, 255))
-                # ignore any window_level_list entry
-            else:
-                # We don't necessarily take the minimum/maximum values, just in case there are outliers
-                # user can specify how much to take off from top and bottom.
-                min_max = np.percentile(
-                    npa.flatten(), intensity_slider_range_percentile
-                )
-                wl_range.append((min_max[0], min_max[1]))
-                if not window_level_list:  # No list was given.
-                    wl_init.append(wl_range[-1])
-                else:
-                    wl = window_level_list[i]
-                    if wl:
-                        wl_init.append((wl[1] - wl[0] / 2.0, wl[1] + wl[0] / 2.0))
-                    else:  # We have a list, but for this image the entry was left empty: []
-                        wl_init.append(wl_range[-1])
-        return (npa_list, wl_range, wl_init)
-
-    def on_slice_slider_value_change(self, change):
-        self.update_display()
-
-    def on_wl_slider_value_change(self, change):
-        self.update_display()
-
-    def update_display(self):
-
-        # Draw the image(s)
-        for ax, npa, title, slider, wl_slider in zip(
-            self.axes, self.npa_list, self.title_list, self.slider_list, self.wl_list
-        ):
-            # We want to keep the zoom factor which was set prior to display, so we log it before
-            # clearing the axes.
-            xlim = ax.get_xlim()
-            ylim = ax.get_ylim()
-
-            self.slc[self.axis] = slice(slider.value, slider.value + 1)
-            ax.clear()
-            # Need to use squeeze to collapse degenerate dimension (e.g. RGB image size 124 124 1 3)
-            ax.imshow(
-                np.squeeze(npa[tuple(self.slc)]),
-                cmap=plt.cm.Greys_r,
-                vmin=wl_slider.value[0],
-                vmax=wl_slider.value[1],
-            )
-            ax.set_title(title)
-            ax.set_axis_off()
-
-            # Set the zoom factor back to what it was before we cleared the axes, and rendered our data.
-            ax.set_xlim(xlim)
-            ax.set_ylim(ylim)
-
-        self.fig.canvas.draw_idle()
