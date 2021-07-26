9916d178ff992569612e374f4b5c0e2c10d19893
sarthakpati <sarthak.pati@hotmail.com>
2021-07-26 10:21:13 -0400

    list change

--- ScreenshotMaker/screenshot_maker.py
+++ ScreenshotMaker/screenshot_maker.py
@@ -5,8 +5,12 @@ from .utils import perform_sanity_check_on_subject
 
 class ScreenShotMaker:
     def __init__(self, images, masks=None, slice_numbers=None, mask_opacity=100):
-        self.images = images
-        self.masks = masks
+
+        self.images = images.split(",")
+        if masks is not None:
+            self.masks = masks.split(",")
+        else:
+            self.masks = None
         self.slice_numbers = slice_numbers
         self.mask_opacity = mask_opacity
 
