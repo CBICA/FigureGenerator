c01268b82e9eecc098841eca1c53ab62329b6e5b
sarthakpati <sarthak.pati@hotmail.com>
2021-07-27 23:25:25 -0400

    this is looking MUCH better (slower, but better)

--- ScreenshotMaker/screenshot_maker.py
+++ ScreenshotMaker/screenshot_maker.py
@@ -78,81 +78,70 @@ class ScreenShotMaker:
             bounding_box = get_bounding_box(input_images[0], None, None)
         print(bounding_box)
 
+        extract = sitk.ExtractImageFilter()
+        extract.SetSize([bounding_box[1] - bounding_box[0] + 1, bounding_box[3] - bounding_box[2] + 1, bounding_box[5] - bounding_box[4] + 1])
+        extract.SetIndex([bounding_box[0], bounding_box[2], bounding_box[4]])
+
         # get the bounded image and masks in the form of arrays
         if len(input_images[0].GetSize()) == 3:
-            self.input_images_array = [
-                np.swapaxes(sitk.GetArrayFromImage(image), 0, 2)[
-                    bounding_box[0] : bounding_box[1],
-                    bounding_box[2] : bounding_box[3],
-                    bounding_box[4] : bounding_box[5],
-                ]
+            self.input_images_bounded = [
+                image[bounding_box[0]:bounding_box[1] + 1, bounding_box[2]:bounding_box[3] + 1, bounding_box[4]:bounding_box[5] + 1]
                 for image in input_images
             ]
             self.image_is_2d = False
         elif len(input_images[0].GetSize()) == 2:
-            raise NotImplementedError("2D not yet supported")
-            # self.input_images_array = [
-            #     sitk.GetArrayFromImage(image)[
-            #         bounding_box[0] : bounding_box[1],
-            #         bounding_box[2] : bounding_box[3],
-            #         :,
-            #     ]
-            #     for image in input_images
-            # ]
-            # self.image_is_2d = True
+            # raise NotImplementedError("2D not yet supported")
+            self.input_images_bounded = [
+                image[bounding_box[0]:bounding_box[1] + 1, bounding_box[2]:bounding_box[3] + 1, :]
+                for image in input_images
+            ]
+            self.image_is_2d = True
 
         if self.mask_present:
             if len(input_images[0].GetSize()) == 3:
-                self.input_masks_array = [
-                    np.swapaxes(sitk.GetArrayFromImage(image), 0, 2)[
-                        bounding_box[0] : bounding_box[1],
-                        bounding_box[2] : bounding_box[3],
-                        bounding_box[4] : bounding_box[5],
-                    ]
+                self.input_masks_bounded = [
+                    image[bounding_box[0]:bounding_box[1] + 1, bounding_box[2]:bounding_box[3] + 1, bounding_box[4]:bounding_box[5] + 1]
                     for image in input_masks
                 ]
             elif len(input_images[0].GetSize()) == 2:
-                self.input_masks_array = [
-                    sitk.GetArrayFromImage(image)[
-                        bounding_box[0] : bounding_box[1],
-                        bounding_box[2] : bounding_box[3],
-                        :,
-                    ]
+                self.input_masks_bounded = [
+                    image[bounding_box[0]:bounding_box[1] + 1, bounding_box[2]:bounding_box[3] + 1, :]
                     for image in input_masks
                 ]
 
             # loop over each axis and get index with largest area
             max_nonzero = 0
             max_id = [0, 0, 0]
-            for xid in range(self.input_masks_array[0].shape[0]):  # for each x-axis
-                current_slice = self.input_masks_array[0][xid, :, :]
-                current_nonzero = np.count_nonzero(current_slice)
+            size = self.input_masks_bounded[0].GetSize()
+            for xid in range(size[0]):  # for each x-axis
+                current_slice = self.input_masks_bounded[0][xid, :, :]
+                current_nonzero = np.count_nonzero(sitk.GetArrayFromImage(current_slice))
                 if current_nonzero > max_nonzero:
                     current_nonzero = max_nonzero
                     max_id[0] = xid
 
             max_nonzero = 0
-            for yid in range(self.input_masks_array[0].shape[1]):  # for each y-axis
-                current_slice = self.input_masks_array[0][:, yid, :]
-                current_nonzero = np.count_nonzero(current_slice)
+            for yid in range(size[1]):  # for each y-axis
+                current_slice = self.input_masks_bounded[0][:, yid, :]
+                current_nonzero = np.count_nonzero(sitk.GetArrayFromImage(current_slice))
                 if current_nonzero > max_nonzero:
                     current_nonzero = max_nonzero
                     max_id[1] = yid
 
             if not (self.image_is_2d):
                 max_nonzero = 0
-                for zid in range(self.input_masks_array[0].shape[2]):  # for each z-axis
-                    current_slice = self.input_masks_array[0][:, :, zid]
-                    current_nonzero = np.count_nonzero(current_slice)
+                for zid in range(size[2]):  # for each z-axis
+                    current_slice = self.input_masks_bounded[0][:, :, zid]
+                    current_nonzero = np.count_nonzero(sitk.GetArrayFromImage(current_slice))
                     if current_nonzero > max_nonzero:
                         current_nonzero = max_nonzero
                         max_id[2] = zid
 
         else:
-            self.input_masks_array = None
+            self.input_masks_bounded = None
             # if mask is not defined, pick the middle of the array
             max_id = (
-                np.around(np.true_divide(self.input_images_array[0].shape, 2))
+                np.around(np.true_divide(self.input_images_bounded[0].shape, 2))
                 .astype(int)
                 .tolist()
             )
@@ -160,31 +149,31 @@ class ScreenShotMaker:
         self.max_id = max_id
         self.get_image_to_write()
 
-    def get_image_and_mask_slices(self, array_list):
+    def get_image_and_mask_slices(self, image_list):
         """
         Function to get the image and mask slices from the input array.
 
         Args:
-            array_list (list of np.array): The array list to get the slices from.
+            image_list (list of SimpleITK.Image): The array list to get the slices from.
 
         Returns:
-            list of list of np.array: The list of list of image and mask slices.
+            list of list of SimpleITK.Image: The list of list of image and mask slices.
         """
         output_slices = []
-        for array in array_list:
+        for image in image_list:
             current_image_slices = []
             if not (self.image_is_2d):
-                current_image_slices.append(array[self.max_id[0], :, :])
-                current_image_slices.append(array[:, self.max_id[1], :])
-                current_image_slices.append(array[:, :, self.max_id[2]])
+                current_image_slices.append(image[self.max_id[0], :, :])
+                current_image_slices.append(image[:, self.max_id[1], :])
+                current_image_slices.append(image[:, :, self.max_id[2]])
             output_slices.append(current_image_slices)
         return output_slices
 
     def get_image_to_write(self):
-        image_slices = self.get_image_and_mask_slices(self.input_images_array)
+        image_slices = self.get_image_and_mask_slices(self.input_images_bounded)
         mask_slices = None
-        if self.input_masks_array is not None:
-            mask_slices = self.get_image_and_mask_slices(self.input_masks_array)
+        if self.input_masks_bounded is not None:
+            mask_slices = self.get_image_and_mask_slices(self.input_masks_bounded)
         else:
             mask_slices = [[None] * len(slice) for slice in image_slices]
 
@@ -193,10 +182,10 @@ class ScreenShotMaker:
         for image_slice in image_slices:
             current_images = []
             for i in range(len(image_slice)):
-                image = sitk.GetImageFromArray(image_slice[i])
+                # image = sitk.GetImageFromArray(image_slice[i])
                 # image = sitk.Compose(image, image, image)
 
-                current_images.append(alpha_blend(image, None))
+                current_images.append(alpha_blend(image_slice[i], None))
 
             images_blended.append(current_images)
 
@@ -205,15 +194,16 @@ class ScreenShotMaker:
             for (image_slice, mask_slice) in zip(image_slices, mask_slices):
                 current_images = []
                 for i in range(len(image_slice)):
-                    image = sitk.GetImageFromArray(image_slice[i])
+                    # image = sitk.GetImageFromArray(image_slice[i])
                     # image = sitk.Compose(image, image, image)
 
                     mask = None
                     if mask_slice[i] is not None:
-                        mask = rescale_intensity(sitk.GetImageFromArray(mask_slice[i]))
+                        mask = mask_slice[i]
+                        # mask = rescale_intensity(sitk.GetImageFromArray(mask_slice[i]))
                         # mask = sitk.Compose(mask, mask, mask)
 
-                    current_images.append(alpha_blend(image, mask))
+                    current_images.append(alpha_blend(image_slice[i], mask))
 
                 images_blended.append(current_images)
 
