0aa3166a05e09ecd5c39731e5efe983bbe050fa4
sarthakpati <sarthak.pati@hotmail.com>
2021-07-28 20:52:51 -0400

    testing blending

--- ScreenshotMaker/utils.py
+++ ScreenshotMaker/utils.py
@@ -186,15 +186,16 @@ def alpha_blend(image, mask=None, alpha=0.5, colormap="jet"):
     # }
 
     if not mask:
-        mask = sitk.Image(image.GetSize(), sitk.sitkFloat32) + 1.0
-        mask.CopyInformation(image)
+        msk = sitk.Image(image.GetSize(), sitk.sitkFloat32) + 1.0
+        msk.CopyInformation(image)
+        msk = sitk.Compose(msk, msk, msk)
     else:
         filter = sitk.LabelToRGBImageFilter()
         # filter.SetColormap(colormap_lut[colormap])
-        mask = filter.Execute(mask)
-        mask = sitk.Cast(mask, sitk.sitkVectorFloat32)
+        msk = filter.Execute(mask)
+        msk = sitk.Cast(msk, sitk.sitkVectorFloat32)
 
-    components_per_pixel = mask.GetNumberOfComponentsPerPixel()
+    # components_per_pixel = mask.GetNumberOfComponentsPerPixel()
     img = sitk.Cast(image, sitk.sitkFloat32)
     # if components_per_pixel > 1:
     #     # img = sitk.Cast(image, sitk.sitkVectorFloat32)
@@ -202,12 +203,66 @@ def alpha_blend(image, mask=None, alpha=0.5, colormap="jet"):
     # else:
     #     img = sitk.Cast(image, mask.GetPixelID())
 
-    if components_per_pixel == 1:
-        return mask * img
-    else:
-        return sitk.Compose(
-            [
-                sitk.VectorIndexSelectionCast(mask, channel) * img # sitk.VectorIndexSelectionCast(img, channel)
-                for channel in range(components_per_pixel)
-            ]
-        )
+    for channel in range(3):
+        tmp = sitk.VectorIndexSelectionCast(msk, channel)
+        test = 1
+    return sitk.Compose(
+        [
+            (alpha * sitk.VectorIndexSelectionCast(msk, channel)) * img # sitk.VectorIndexSelectionCast(img, channel)
+            for channel in range(3)
+        ]
+    )
+
+
+# def mask_image_multiply(mask, image):
+#     components_per_pixel = image.GetNumberOfComponentsPerPixel()
+#     if components_per_pixel == 1:
+#         temp = mask * image
+#         return sitk.Compose(temp,temp,temp)
+#     else:
+#         return sitk.Compose(
+#             [
+#                 mask * sitk.VectorIndexSelectionCast(image, channel)
+#                 for channel in range(components_per_pixel)
+#             ]
+#         )
+
+
+# def alpha_blend(image1, image2=None, mask1=None, mask2=None, alpha=0.5):
+#     """
+#     Alaph blend two images, pixels can be scalars or vectors.
+#     The region that is alpha blended is controled by the given masks.
+#     """
+
+#     if not mask1:
+#         mask1 = sitk.Image(image1.GetSize(), sitk.sitkFloat32) + 1.0
+#         mask1.CopyInformation(image1)
+#     else:
+#         mask1 = sitk.Cast(mask1, sitk.sitkFloat32)
+#     if not mask2:
+#         mask2 = sitk.Image(image1.GetSize(), sitk.sitkFloat32) + 1
+#         mask2.CopyInformation(image1)
+#     else:
+#         mask2 = sitk.Cast(mask2, sitk.sitkFloat32)
+
+#     if not image2:
+#         image2 = image1
+
+#     components_per_pixel = image1.GetNumberOfComponentsPerPixel()
+#     if components_per_pixel > 1:
+#         img1 = sitk.Cast(image1, sitk.sitkVectorFloat32)
+#         img2 = sitk.Cast(image2, sitk.sitkVectorFloat32)
+#     else:
+#         img1 = sitk.Cast(image1, sitk.sitkFloat32)
+#         img2 = sitk.Cast(image2, sitk.sitkFloat32)
+
+#     intersection_mask = mask1 * mask2
+
+#     intersection_image = mask_image_multiply(
+#         alpha * intersection_mask, img1
+#     ) + mask_image_multiply((1 - alpha) * intersection_mask, img2)
+#     return (
+#         intersection_image
+#         + mask_image_multiply(mask2 - intersection_mask, img2)
+#         + mask_image_multiply(mask1 - intersection_mask, img1)
+#     )
