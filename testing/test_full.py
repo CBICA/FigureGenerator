235f55b057d87e146da6d108ffbb2481df54baa6
sarthakpati <sarthak.pati@hotmail.com>
2021-08-03 09:55:22 -0400

    black.

--- testing/test_full.py
+++ testing/test_full.py
@@ -1,14 +1,23 @@
 from pathlib import Path
-import requests, zipfile, io, os
+import requests, zipfile, io, os, argparse
 
 from FigureGenerator.screenshot_maker import FigureGenerator
 
 ## global variables
 inputDir = os.path.abspath(os.path.normpath("./testing/data"))
-args = {}
-args.images = os.join(inputDir, "fl.nii.gz") + "," + os.join(inputDir, "t1.nii.gz") + "," + os.join(inputDir, "t2.nii.gz") + "," + os.join(inputDir, "t1ce.nii.gz")
-args.masks = os.join(inputDir, "seg.nii.gz")
-args.output = os.join(inputDir, "output.nii.gz")
+args = argparse.Namespace
+args.images = (
+    os.path.join(inputDir, "fl.nii.gz")
+    + ","
+    + os.path.join(inputDir, "t1.nii.gz")
+    + ","
+    + os.path.join(inputDir, "t2.nii.gz")
+    + ","
+    + os.path.join(inputDir, "t1ce.nii.gz")
+)
+args.masks = os.path.join(inputDir, "seg.nii.gz")
+args.output = os.path.join(inputDir, "output.nii.gz")
+
 
 def test_download_data():
     """
@@ -16,7 +25,7 @@ def test_download_data():
     """
     urlToDownload = "https://github.com/CBICA/FigureGenerator/raw/main/testing/data.zip"
     # do not download data again
-    if not Path(os.join(inputDir, "fl.nii.gz")).exists():
+    if not Path(os.path.join(inputDir, "fl.nii.gz")).exists():
         print("Downloading and extracting sample data")
         r = requests.get(urlToDownload)
         z = zipfile.ZipFile(io.BytesIO(r.content))
